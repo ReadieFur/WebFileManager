cb3277361a8834e865de7b80f7e2b9a5fdb605e9
Tristan Read <60425965+kOFReadie@users.noreply.github.com>
2021-12-27 21:44:33 +0000

    Misc changes

---
+++ dist.sh
@@ -0,0 +1,4 @@
+#https://askubuntu.com/questions/333640/cp-command-to-exclude-certain-files-from-being-copied
+rsync -avr src/ dist/ --exclude '_assets/configuration/config.json' --exclude '_assets/libs/vendor';
+zip -r dist.zip dist/;
+rm -rf dist;
